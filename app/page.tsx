b84d30b558e53246d1f729f16184243982bac874
TimAaronSharp <timothyaaron.sharp@gmail.com>
2026-08-24 16:40:01 -0600

    Chapter 2 complete - Teaching how to use Tailwind and CSS modules. It mentioned "clsx" library to conditionally style an element based on state or other conditions, but did not have me incorporate that yet.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import styles from '@/app/ui/home.module.css'
+/*NOTE import styles from '@/app/ui/home.module.css' is for using CSS Modules, which allow you to scope css to a component.
+They are using Tailwind for the tutorial but showed how to do this. After importing that css file you can access it like,
+<div className={styles.shape} /> (shape being the name of the css class). This css class has the same rules that the 
+<div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"/>
+line has.*/
 
 export default function Page() {
   return (
@@ -10,6 +16,9 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+          <div
+            className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
+          />
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
