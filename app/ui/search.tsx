b963f74b0fedc2949414ed8ebfa17ccacf5cb579
TimAaronSharp <timothyaaron.sharp@gmail.com>
2026-08-29 16:50:38 -0600

    Chapter 10 complete - Teaching about adding search (along with debouncing) and pagination capabilities.
    Fleshed out "app/dashboard/invoices/page.tsx" and incorporated "search.tsx" and "pagination.tsx" components/sub-components.

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,31 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, useRouter, usePathname } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  /*NOTE useSearchParams() returns a read-only object with the search params in the URL.*/
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback((term: string) => {
+    /*NOTE URLSearchParams is a Web API utility class designed to make reading, editing, 
+    and formatting query strings easy. By passing it the read-only "searchParams" object
+    we are creating a new instance that can be manipulated with the read-only data passed in.*/
+
+    console.log(`Searching... ${term}`);
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +34,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
